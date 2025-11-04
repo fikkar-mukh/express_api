00e02c704c05116c7c804f9ffff0daef823ab48b
Fikkar Mukhtar <fikkartaufiqy@gmail.com>
2025-11-04 22:42:28 +0700

    4th commit

--- config/db.js
+++ config/db.js
@@ -1,10 +1,16 @@
 import mysql from "mysql2/promise";
 
+import dotenv from "dotenv";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "test_nutech",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
 
 export default db;
